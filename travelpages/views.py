e43eb3c2f4d4e16db882da3f29f0d94bf566288d
Tyler Sheffield <tylersheffield427@gmail.com>
2023-03-31 18:53:30 -0600

    commit

--- travelpages/views.py
+++ travelpages/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
-def indexPageView(request) :
-    return render(request, 'index.html/')
+
+def indexPageView(request):
+    lstData = [
+        {
+            "last_name": "Doe",
+            "first_name": "John",
+            "gpa": 3.87,
+            "major": "Accounting",
+        },
+        {
+            "last_name": "Lamb",
+            "first_name": "Mary",
+            "gpa": 3.91,
+            "major": "Information Systems",
+        },
+        {
+            "last_name": "Wheat",
+            "first_name": "Buck",
+            "gpa": 3.70,
+            "major": "Marketing",
+        },
+        {
+            "last_name": "Anderson",
+            "first_name": "Greg",
+            "gpa": 2.99,
+            "major": "Fun",
+        },
+    ]
+
+    context = {
+        'school_name': 'BYU',
+        'degrees': ["IS", "ACCT", "MKTG"],
+        'data': lstData
+    }
+
+    return render(request, 'travelpages/index.html', context)
